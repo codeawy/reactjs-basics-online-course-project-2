6745fde68a2e89fa931317a7fe6844e7489dac1f
codeawy <codeawy@gmail.com>
2023-08-14 17:13:12 +0300

    Enhance add product modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import ProductCard from "./components/ProductCard";
 import Button from "./components/ui/Button";
+import Input from "./components/ui/Input";
 import Modal from "./components/ui/Modal";
-import { productList } from "./data";
+import { formInputsList, productList } from "./data";
 import { useState } from "react";
 
 const App = () => {
@@ -19,6 +20,14 @@ const App = () => {
 
   /* ------- RENDER -------  */
   const renderProductList = productList.map(product => <ProductCard key={product.id} product={product} />);
+  const renderFormInputList = formInputsList.map(input => (
+    <div className="flex flex-col">
+      <label htmlFor={input.id} className="mb-[2px] text-sm font-medium text-gray-700">
+        {input.label}
+      </label>
+      <Input type="text" id={input.id} name={input.name} />
+    </div>
+  ));
 
   return (
     <main className="container">
@@ -29,9 +38,12 @@ const App = () => {
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="ADD A NEW PRODUCT">
-        <div className="flex items-center space-x-3">
-          <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
-          <Button className="bg-gray-300 hover:bg-gray-400">Cancel</Button>
+        <div className="space-y-3">
+          {renderFormInputList}
+          <div className="flex items-center space-x-3">
+            <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
+            <Button className="bg-gray-400 hover:bg-gray-500">Cancel</Button>
+          </div>
         </div>
       </Modal>
     </main>
